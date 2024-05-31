bb77a62ca10ca84b081429672945ff97eddedd6a
GoruAkiba <goru.akiba@gmail.com>
2024-05-31 23:35:16 +0700

    downgrade nodefetch

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+const fetch = require("node-fetch");
 import _request from "request";
 import express from "express";
 
--- stDownloader.js
+++ stDownloader.js
@@ -1,4 +1,3 @@
-import fetch from "node-fetch";
 export class stdownloader {
     constructor(){
         this.regex = /document\.getElementById\(\'norobotlink\'\)\.innerHTML \= ([\d a-zA-Z0-9!@#$%^&*()_+\-=\[\]{}':"\\|,.<>\/?]*)\;/;
